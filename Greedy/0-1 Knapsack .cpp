279cd7cfdf4715e37df4c4823d20aa21e7d2fa64
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2018-10-11 19:38:49 +0600

    Update 0-1 Knapsack .cpp

--- Greedy/0-1 Knapsack .cpp
+++ Greedy/0-1 Knapsack .cpp
@@ -1,40 +1,44 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
+#include <climits>
 using namespace std;
 
- pair<int,int> pi[1000];
- int i,n,w,res=0,weight,price;
+// Values (stored in array val)
+// Weights (stored in array wet)
+// Number of distinct items (n)
+// Knapsack capacity capasity
+
+int val[100000];
+int wet[100000];
+int benefit,benefit2,capasity,n,i;
+
+int knapSack(int n, int capasity)
+{
+
+    if (capasity < 0)    /// base case: Negative capacity
+        return INT_MIN;  /// base case: no items left or capacity becomes 0
+
+
+    if (n < 0 || capasity == 0)
+        return 0;
+
+
+    int benefit = val[n] + knapSack(n - 1, capasity - wet[n]);  /// Case 1. include current item n in knapSack (v[n]) and recurse for                                                                /// remaining items (n - 1) with decreased capacity (W - w[n])
+
+    int benefit2 = knapSack(n - 1, capasity);             /// Case 2. exclude current item n from knapSack and recurse for remaining items (n - 1)
+
+    return max (benefit, benefit2);                  /// return maximum value we get by including or excluding current item
+}
 
 int main()
 {
-    freopen("in.txt","r",stdin);
-
-    puts("Enter the number and weight");
-    cin>>n>>w; ///n is num or item
-
+   // freopen("in.txt","r",stdin);
+    cin>>n>>capasity;
     for(i=0;i<n;i++)
-    {
-       cin>>weight>>price;
-       pi[i].first=price;
-       pi[i].second=weight;
-    }
+     {
+         cin>>val[i]>>wet[i];    /// val and weight
+     }
 
-    sort(pi,pi+n); ///Ascending order.
-
-    for(i=n-1;i>=0; i--) ///Read data in descending order while the weight is greater than 0
-    {
-       if((cur+pi[i].second)<=w)          ///when weight is 0 then loop end.
-      {
-        res+=pi[i].first;
-        cur+=pi[i].second;
-      }
-    }
-    cout<<"Max profit:"<<res<<endl;
-
-    /*for(i=n-1;i>=0;i--)
-    {
-      cout<<pi[i].first<<" "<<pi[i].second<<endl;
-    }*/
+    cout << "Maximum Knapsack value is : "<< knapSack(n - 1, capasity)<<endl;
 
     return 0;
 }
