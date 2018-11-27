ffb718b97b0b39244f312900d16c2662dd9a5afc
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2018-11-27 22:24:04 +0600

    Update Euler titient function.cpp

--- Number Theory/Euler titient function.cpp
+++ Number Theory/Euler titient function.cpp
@@ -2,27 +2,22 @@
 #define mx 100000000
 using namespace std;
 
-int mark[mx+1],phi[mx+2];
+int phi[mx+2];
 
 void Euler_totient(int n)
 {
   int i,j;
   for(i=1;i<=n;i++){phi[i]=i;}
 
-  phi[1]=1;
-  mark[1]=1;
-
   for(i=2;i<=n;i++)
   {
-    if(!mark[i])
+    if(phi[i]==i)  /// i is a prime
     {
       for(j=i;j<=n;j+=i)
       {
-        mark[j]=1;
-        phi[j]=(phi[j]/i)*(i-1);
-
+       /// phi[j]=(phi[j]/i)*(i-1);
+        phi[j] -= phi[j] / i;
       }
-
     }
   }
 
@@ -33,14 +28,10 @@ int main()
 {
   int n;
   cin>>n;
-
   Euler_totient(n);
 
    for(int i=1;i<=n;i++)
    {
      cout<<i<<": "<<phi[i]<<endl;
-
-
    }
-
 }
