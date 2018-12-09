a72234993ab5ee9f23077c34e9536c2b0833c0c0
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2018-12-09 19:24:22 +0600

    Update Sieve  Eratosthenes.cpp

--- Number Theory/Sieve  Eratosthenes.cpp
+++ Number Theory/Sieve  Eratosthenes.cpp
@@ -1,57 +1,55 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define mx 10000000
+
 
 using namespace std;
-typedef long long ll;
-int main()
- {
 
-  int n,m;
-  printf("Enter the limit for check prime :\n");
-  cin>>n;
+typedef  long long int ll;
 
-  int p=sqrt(n)+1;
-  int prime[n+2];
-  int check[n];
-  int cnt=1;
+ll num,sq,x,i,j,cnt,len,p,n;
+bool check[mx+1];
+vector<ll> prime;
 
+void prime_test()
+{
+  prime.push_back(2);
+   p=sqrt(mx)+1;
 
-
-    for(int i=4;i<=n;i+=2)///All even except 3 are marked.
+ for(i=4;i<=mx;i+=2)///All even except 3 are marked.
+  {
+     check[i]=1;
+  }
+    for(int i=3;i<=mx;i+=2)///same sqrt(n)==i*i
     {
-     prime[i]=0;
-    }
-    for(int i=3;i<=n;i+=2)///same sqrt(n)==i*i
-    {
-      if(prime[i]!=0)
+      if(check[i]==0)
       {
-          check[++cnt]=i; ///i means prime
+          prime.push_back(i); ///i means prime
           if(i<=p)  ///there is no divisor after sqrt n
           {
-           for(int j=i*2;j<=n;j+=i)
+            for(int j=i*2;j<=mx;j+=i)
            {
-              prime[j]=0; ///0 means not prime
+              check[j]=1; ///0 means not prime
            }
           }
       }
     }
-    check[1]=2;
+}
 
- cout<<"Prime series is :";
 
- for(int i=1;i<=cnt;i++)
-   {
-     cout<<check[i]<<" ";
-   }
 
-   cout<<endl<<"Enter any number to check whether it is prime or not:";
-   cin>>m;
+int main()
+{
+  prime_test();
 
-   if(prime[m])
-         printf("%d is a prime number",m);
-    else
-         printf("%d is not a prime number",m);
+ cin>>n;
 
-     return 0;
- }
+  for(i=0;i<n;i++)
+  {
+    cout<<prime[i]<<" ";
+  }
+
+    return 0;
+}
+
 
 
