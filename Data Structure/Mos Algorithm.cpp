21b3a8f0e581996a025ef21006b1b161da70ed1a
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2019-05-10 22:44:26 +0600

    Update Mos Algorithm.cpp

--- Data Structure/Mos Algorithm.cpp
+++ Data Structure/Mos Algorithm.cpp
@@ -2,7 +2,9 @@
 #define mx 100000
 using namespace std;
 
-int res[mx];
+int currL = 0,currR =-1;
+int sum = 0;
+int res[mx],arr[mx];
 
 int block;
 
@@ -13,79 +15,51 @@ struct Query
 
 bool compare(Query x,Query y)
 {
-    /// Different blocks, sort by block.
-    if (x.L/block != y.L/block)
-        return x.L/block < y.L/block;
-    /// Same block, sort by R value
-    return x.R < y.R;
+
+    if (x.L/block != y.L/block)  /// Different blocks, sort by block.
+      return x.L/block < y.L/block;
+
+    return x.R < y.R; /// Same block, sort by R value
 }
 
+void Add(int x) { sum += arr[x];}
+void Remove(int x) { sum -= arr[x];}
 
-void queryResults(int a[], int n, Query q[], int m)
+
+void queryResults(int arr[], int n, Query q[], int m)
 {
     block = (int)sqrt(n);
-    // Sort all queries so that queries of same blocks
-    // are arranged together.
     sort(q, q + m, compare);
 
-  /*  for(int i=0;i<m;i++)
+  /*for(int i=0;i<m;i++)
 	{
 	 cout<<q[i].L<<" "<<q[i].R<<" "<<q[i].id<<endl;
-
-	}*/
-
-    int currL = 0, currR = 0;
-    int Sum = 0;
+	}
+	*/
 
     for (int i=0; i<m; i++)
     {
-        // L and R values of current range
-        int L = q[i].L, R = q[i].R;
-        // Remove extra elements of previous range. For
-        // example if previous range is [0, 3] and current
-        // range is [2, 5], then a[0] and a[1] are subtracted
-        while (currL < L)
-        {
-            Sum-= a[currL];
-            currL++;
-        }
-        /// Add Elements of current Range
-        while (currL > L)
-        {
-            Sum += a[currL-1];
-            currL--;
-        }
-        while (currR <= R)
-        {
-            Sum += a[currR];
-            currR++;
-        }
-        // Remove elements of previous range.  For example
-        // when previous range is [0, 10] and current range
-        // is [3, 8], then a[9] and a[10] are subtracted
-        while (currR > R+1)
-        {
-            Sum -= a[currR-1];
-            currR--;
-        }
-            // Print sum of current range
-            // cout << "Sum of [" << L << ", " << R
-            // << "] is "  << Sum << endl;
-        res[q[i].id]=Sum;
+        int l = q[i].L, r = q[i].R;
+
+      while(currL > l) Add(--currL);
+      while(currR < r) Add(++currR);
+      while(currL < l) Remove(currL++);
+      while(currR > r) Remove(currR--);
+
+     res[q[i].id]=sum;
+
     }
 }
 
 
 int main()
 {
-    freopen("in.txt","r",stdin);
+    //freopen("in.txt","r",stdin);
+	Query q[100000];
+	int n,m;
 
-	int a[] = {1, 1, 2, 1, 3, 4, 5, 2, 8};
-	int n = sizeof(a)/sizeof(a[0]);
-
-	Query q[100];
-
-	int m;
+	cin>>n;
+	for(int i=0;i<n;i++) cin>>arr[i];
 
 	cin>>m;
 
@@ -95,7 +69,7 @@ int main()
 	  q[i].id=i;
 	}
 
-	queryResults(a, n, q, m);
+	queryResults(arr, n, q, m);
 
 	for(int i=0;i<m;i++)
 	{
