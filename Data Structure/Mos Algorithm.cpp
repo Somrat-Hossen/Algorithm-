9cd3a3446284e6b8f47392c1df4b1494c4782746
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2019-10-04 00:53:48 +0600

    Update Mos Algorithm.cpp

--- Data Structure/Mos Algorithm.cpp
+++ Data Structure/Mos Algorithm.cpp
@@ -1,32 +1,40 @@
 #include <bits/stdc++.h>
-#define mx 100000
+#define mx 30005
 using namespace std;
 
-int currL = 0,currR =-1;
+typedef long long ll;
+
+int currL = 0,currR =0;
 int sum = 0;
-int res[mx],arr[mx];
+int res[2*100005],arr[mx],vis[1000005];
 
 int block;
 
 struct Query
 {
 	int L, R,id;
-};
+}q[200005];
 
 bool compare(Query x,Query y)
 {
-
+    /// of the same block are arranged together and within a block,
+     /// queries are sorted in increasing order of R values.
     if (x.L/block != y.L/block)  /// Different blocks, sort by block.
       return x.L/block < y.L/block;
 
     return x.R < y.R; /// Same block, sort by R value
 }
 
-void Add(int x) { sum += arr[x];}
-void Remove(int x) { sum -= arr[x];}
+void Add(int x)
+{
+  sum+=arr[x];
+}
+void Remove(int x)
+{
+  sum-=arr[x];
+}
 
-
-void queryResults(int arr[], int n, Query q[], int m)
+void queryResults(int n, int m)
 {
     block = (int)sqrt(n);
     sort(q, q + m, compare);
@@ -41,10 +49,10 @@ void queryResults(int arr[], int n, Query q[], int m)
     {
         int l = q[i].L, r = q[i].R;
 
-      while(currL > l) Add(--currL);
-      while(currR < r) Add(++currR);
       while(currL < l) Remove(currL++);
-      while(currR > r) Remove(currR--);
+      while(currL > l) Add(currL-1),currL--;
+      while(currR <= r) Add(currR++);
+      while(currR > r+1) Remove(currR-1),currR--;
 
      res[q[i].id]=sum;
 
@@ -55,26 +63,35 @@ void queryResults(int arr[], int n, Query q[], int m)
 int main()
 {
     //freopen("in.txt","r",stdin);
-	Query q[100000];
-	int n,m;
 
+   ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cerr.tie(NULL);
+
+
+   int n,m;
 	cin>>n;
+
 	for(int i=0;i<n;i++) cin>>arr[i];
 
-	cin>>m;
+	cin>>m;///num or query
 
 	for(int i=0;i<m;i++)
-	{
-	  cin>>q[i].L>>q[i].R;
-	  q[i].id=i;
-	}
+   {
+      cin>>q[i].L>>q[i].R;
+      q[i].id=i;
+      q[i].L--;///o indexed is my code but given query is 1 based.
+      q[i].R--;
+   }
 
-	queryResults(arr, n, q, m);
+   queryResults(n,m);
 
-	for(int i=0;i<m;i++)
-	{
-	   cout<<res[i]<<" ";
-	}
+   for(int i=0;i<m;i++)
+   {
+      cout<<res[i]<<endl;
+   }
 
   return 0;
 }
+
